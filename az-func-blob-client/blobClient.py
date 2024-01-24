04ad20d8c69fa37a9c63d9153a8ae31f268c2d53
Victoria Hall <victoriahall@microsoft.com>
2024-01-24 13:48:57 -0600

    prototype start

--- az-func-blob-client/blobClient.py
+++ az-func-blob-client/blobClient.py
@@ -7,20 +7,9 @@ from typing import Any, Optional, Union
 
 # DATA IS MODEL_BINDING_DATA
 
-class BlobClient():
-    # Change constructor to only take in data -- parse to get connection string, container name, and blob name
-    def __init__(self, *, data: Union[bytes, Datum],
-                 name: Optional[str] = None,
-                 uri: Optional[str] = None,
-                 length: Optional[int] = None,
-                 blob_properties: Optional[dict] = None,
-                 metadata: Optional[dict] = None) -> None:
-        # Blob properties
-        self._name = name
-        self._length = length
-        self._uri = uri
-        self._blob_properties = blob_properties
-        self._metadata = metadata
+class BlobClient(SdkType):
+    # is the type of data correct here?
+    def __init__(self, *, data: Union[bytes, Datum]) -> None:
 
         # model_binding_data properties
         self._data = data or {}
@@ -39,25 +28,7 @@ class BlobClient():
             self._containerName = content_json["ContainerName"]
             self._blobName = content_json["BlobName"]
 
-    @property
-    def name(self) -> Optional[str]:
-        return self._name
-
-    @property
-    def length(self) -> Optional[int]:
-        return self._length
-
-    @property
-    def uri(self) -> Optional[str]:
-        return self._uri
-
-    @property
-    def blob_properties(self):
-        return self._blob_properties
-
-    @property
-    def metadata(self):
-        return self._metadata
+    # no getters b/c not exposing any other properties
 
     # THIS IS WHERE WE RETURN THE CLIENT
     def get_sdk_type(self):
